f8897a0384762419186c69544053314b7a02a122
GK <gisela0108@github.com>
2018-04-27 11:35:30 +0700

    IDLE

--- app/src/main/java/com/gisela/pointofsales/MainActivity.java
+++ app/src/main/java/com/gisela/pointofsales/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity
                 MainActivity.this.startActivity(intent);
             }
         });
-
+        
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
